ced9db411456a5fc53a0cab53f1f40283cf49c4d
花生 <bjlidachuan@163.com>
2016-03-19 11:32:23 +0800

    Initial Commit

---
+++ LDCTransitionAnimatorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDCTransitionAnimatorDemo
+//
+//  Created by 花生 on 16/3/19.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LDCTransitionAnimatorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LDCTransitionAnimatorDemo
+//
+//  Created by 花生 on 16/3/19.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
